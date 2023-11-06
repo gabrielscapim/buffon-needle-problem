6b9f92a5284b369db684493b940b444b06be6213
Gabriel <scapimgabriel@gmail.com>
2023-11-06 15:01:04 -0300

    fix: comments in the code

--- script.ts
+++ script.ts
@@ -1,9 +1,9 @@
-// Probabilidade de cruzar = 2 * l/pi * d
+// Probabilidade de cruzar = 2 * l/pi * d, com l = comprimento da agulha e d = distância entre linhas
 // pi = 2 * l / p * d
 
 // Para que cruze
-// L * sen(w) * 0.5 >= x, sendo x = distância entre o ponto central do objeto à paralela mais próxima
-// L * sen(w) * 0.5 - x >= 0
+// l * sen(w) * 0.5 >= x, sendo x = distância entre o ponto central do objeto à paralela mais próxima
+// l * sen(w) * 0.5 - x >= 0
 
 const needleBuffon = (needleWidth: number, linesDistance: number): number | string => {
   if (needleWidth * 2 !== linesDistance) return "A proporção entre a distância entre as linhas e o comprimento da agulha deve ser igual a 2."
@@ -11,10 +11,10 @@ const needleBuffon = (needleWidth: number, linesDistance: number): number | stri
   const needlesPosition: number[] = [];
   const needlesAngles: number[] = [];
   const monteCarlo: boolean[] = [];
-  const RUN_TIMES: number = 1000000;
+  const RUN_TIMES: number = 100000;
 
   for (let index = 0; index < RUN_TIMES; index += 1) {
-    needlesPosition.push(Math.random() * (linesDistance / 2)); // A distância entre o centro da agulha e a linha mais próxima está entre 0 e l/2
+    needlesPosition.push(Math.random() * (linesDistance / 2)); // A distância entre o centro da agulha e a linha mais próxima está entre 0 e d/2
     needlesAngles.push(Math.random() * (Math.PI / 2)); // O ângulo da agulha está entre 0 e 90 graus em relação a linha
     monteCarlo.push(needleWidth * Math.sin(needlesAngles[index]) * 0.5 - needlesPosition[index] >= 0);
   }
